4228dc4f0b989aa9ce23464a1be6e4ec8a4a0e14
Minho Yi <tisohjung@gmail.com>
2021-05-27 00:43:50 +0900

    Start with VCNewsList

---
+++ RxNews/VCNewsList.swift
@@ -0,0 +1,15 @@
+//
+//  VCNewsList.swift
+//  RxNews
+//
+//  Created by minho on 2021/05/27.
+//
+
+import Foundation
+import UIKit
+
+class VCNewsList: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
